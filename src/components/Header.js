03abb61d5d1898b33cde73f2fed77da014e2a886
denis salvino <denis.salvino23@gmail.com>
2021-05-09 22:26:01 -0500

    added sort

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Header = () => {
+    return (
+        <div className="container-fluid headerContainer">
+            <div id="header" className="pt-5 pb-3 text-center block block-2">
+                <h1 className="mb-5">Company Employee Directory</h1>
+            </div>
+        </div>
+
+    );
+}
+
+export default Header;
